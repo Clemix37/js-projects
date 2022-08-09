5d9354ec18a936b0a51a319b15d290269f592d15
H4ckW1s3r <clementix01@gmail.com>
2022-08-09 14:58:50 +0200

    added chaining in Counter

--- projects/01-counter/Counter.js
+++ projects/01-counter/Counter.js
@@ -4,7 +4,7 @@ class Counter {
         this.#points = 0;
     }
     get points(){return this.#points;}
-    increase(){this.#points++;}
-    decrease(){this.#points--;}
+    increase(){this.#points++;return this;}
+    decrease(){this.#points--;return this;}
     reset(){this.#points=0;return this;}
 }
