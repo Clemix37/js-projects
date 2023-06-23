f173d8670baaa609f2e79080c0477ebe2f5f0dc3
H4ckW1s3r <clementix01@gmail.com>
2023-06-23 11:29:56 +0200

    Added playing instruments from key

--- projects/drum-kit/app.js
+++ projects/drum-kit/app.js
@@ -1,12 +1,23 @@
 let btns = document.querySelectorAll('.button');
+const getBtnFromKey = (key) => Array.from(btns).filter(btn => btn.dataset.key === key)?.[0] ?? null;
+const getInstrumentFromEvent = (e) => e.currentTarget.dataset.instrument ?? null;
+
 for (let i = 0; i < btns.length; i++) {
     const btn = btns[i];
-    btn.addEventListener('click', ()=>{
-        launch(btn.innerHTML.toLowerCase());
+    btn.addEventListener('click', (e) => {
+        const instrument = getInstrumentFromEvent(e);
+        launch(instrument);
     });
 }
 
-function launch(idPlayer){
+function launch(idPlayer = null){
+    if(!idPlayer) return;
     let audio = document.getElementById(idPlayer);
     audio.play();
 }
+
+
+document.addEventListener('keydown', (e) => {
+    const instrument = getBtnFromKey(e.key)?.dataset.instrument;
+    launch(instrument);
+});
