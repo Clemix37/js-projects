6485122cc08d89ee8091f164f7618ae7fac6c0fd
H4ckW1s3r <clementix01@gmail.com>
2024-03-31 23:50:03 +0200

    Changed number guesser to module, added regeneration

--- projects/number-guesser/app.js
+++ projects/number-guesser/app.js
@@ -1,22 +1,32 @@
-(function(){
-    let nbToFind = 0;
-    let nb = document.getElementById('nb');
-    let btnSearch = document.getElementById('btnSearch');
-    let results = document.getElementById('results');
-    document.addEventListener('DOMContentLoaded',()=>{
-        nbToFind = Math.floor(Math.random()*100);
-    });
-    nb.addEventListener('keyup', (e)=>{
-        if(e.key == "Enter") search();
-    });
-    btnSearch.addEventListener('click', search);
+// PROPERTIES
+let nbToFind = 0;
+let nb = document.getElementById('nb');
+let btnSearch = document.getElementById('btnSearch');
+let results = document.getElementById('results');
 
-    function search(){
-        let search = parseInt(nb.value);
-        if(search==nbToFind) results.innerHTML = `You won !`;
-        else if(search<nbToFind) results.innerHTML = `Greater`;
-        else if(search>nbToFind) results.innerHTML = `Lower`;
-        else results.innerHTML = `Not a number`;
-        nb.value = ``;
+// EVENTS
+nb.addEventListener('keyup', (e)=>{
+    if(e.key == "Enter") search();
+});
+btnSearch.addEventListener('click', search);
+
+// FUNCTIONS
+function search(){
+    let search = +nb.value;
+    if(search == nbToFind) {
+        results.textContent = `You won ! A new number has been generated if you want to continue ;)`;
+        // We regenerate a number to guess
+        genNumberTofind();
     }
-})();
+    else if(search < nbToFind) results.textContent = `Greater`;
+    else if(search > nbToFind) results.textContent = `Lower`;
+    else results.textContent = `Not a number`;
+    nb.value = ``;
+}
+
+function genNumberTofind(){
+    nbToFind = Math.floor(Math.random()*100);
+}
+
+// After loading, we generate a random number
+genNumberTofind();
