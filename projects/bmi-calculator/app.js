0c2c457c8d09ef1e864f817fbc1f7b26e3bf98b5
H4ckW1s3r <clementix01@gmail.com>
2024-03-31 23:57:27 +0200

    Organized bmi calculator project

--- projects/bmi-calculator/app.js
+++ projects/bmi-calculator/app.js
@@ -1,29 +1,40 @@
+// PROPERTIES
 const results = document.getElementById('results');
-const taille = document.getElementById('taille');
-const poids = document.getElementById('poids');
+const height = document.getElementById('taille');
+const weight = document.getElementById('poids');
 const btnCalculate = document.getElementById('btnCalculate');
-const cleEntre = "Enter";
+const enterKey = "Enter";
 
-btnCalculate.addEventListener('click', calculer);
-taille.addEventListener('keyup', (e)=>{
-    if(e.key == cleEntre) calculer();
+// EVENTS
+btnCalculate.addEventListener('click', calculate);
+height.addEventListener('keyup', (e)=>{
+    if(e.key == enterKey) calculate();
 });
-poids.addEventListener('keyup', (e)=>{
-    if(e.key == cleEntre) calculer();
+weight.addEventListener('keyup', (e)=>{
+    if(e.key == enterKey) calculate();
 });
 
-function calculer(){
-    const valeurTaille = parseInt(taille.value);
-    const valeurPoids = parseFloat(poids.value);
-    if(!valeurTaille || !valeurPoids) return;
-    const calcul = (valeurPoids / ((valeurTaille/100)**2));
-    let resultat = "";
-    if(calcul < 16.5) resultat = `Malnutrition`;
-    else if(calcul >= 16.5 && calcul < 18.5) resultat = `Thinness`;
-    else if(calcul >= 18.5 && calcul < 25) resultat = `Normal weight`;
-    else if(calcul >= 25 && calcul < 30) resultat = `Overweight`;
-    else if(calcul >= 30 && calcul < 35) resultat = `Moderate obesity`;
-    else if(calcul >= 35 && calcul < 40) resultat = `Severe obesity`;
-    else if(calcul >= 40) resultat = `Morbid obesity`;
-    results.innerHTML = `<h3 class="subtitle has-text-centered">${resultat}</h3>`;
+// FUNCTIONS
+function calculate(){
+    const heightValue = +height.value;
+    const weightValue = +weight.value;
+    if(!heightValue || !weightValue) return;
+    const calculation = weightValue / ((heightValue/100)**2);
+    displayResultBasedOnCalculation(calculation);
+}
+
+/**
+ * Display the result based on the calculation givent as argument
+ * @param {number} calculation 
+ */
+function displayResultBasedOnCalculation(calculation){
+    let result = "";
+    if(calculation < 16.5) result = `Malnutrition`;
+    else if(calculation >= 16.5 && calculation < 18.5) result = `Thinness`;
+    else if(calculation >= 18.5 && calculation < 25) result = `Normal weight`;
+    else if(calculation >= 25 && calculation < 30) result = `Overweight`;
+    else if(calculation >= 30 && calculation < 35) result = `Moderate obesity`;
+    else if(calculation >= 35 && calculation < 40) result = `Severe obesity`;
+    else if(calculation >= 40) result = `Morbid obesity`;
+    results.innerHTML = `<h3 class="subtitle has-text-centered">${result}</h3>`;
 }
