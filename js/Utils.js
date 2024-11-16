c0efaf73e7c62282e25c48c6d201d1f64c2e9471
H4ckW1s3r <clementix01@gmail.com>
2024-11-16 13:29:06 +0100

    Added notify in utils so that we can notify user when countdown is finished

--- js/Utils.js
+++ js/Utils.js
@@ -1,114 +1,148 @@
 export default class Utils {
+	//#region Properties
 
-    //#region Properties
+	static dateFormats = {
+		DayMonthYearHyphens: "dd-mm-YYYY",
+		DayMonthYearSlash: "dd/mm/YYYY",
+	};
+	static days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
+	static months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
 
-    static dateFormats = {
-        DayMonthYearHyphens: "dd-mm-YYYY",
-        DayMonthYearSlash: "dd/mm/YYYY",
-    };
-    static days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
-    static months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	//#endregion
 
-    //#endregion
+	//#region Constructor
 
-    //#region Constructor
+	//#endregion
 
-    //#endregion
+	//#region Accessors
 
-    //#region Accessors
+	//#endregion
 
-    //#endregion
+	//#region Public methods
 
-    //#region Public methods
+	/**
+	 * Get a random index from the array given as argument
+	 * @param {any[]} arr
+	 * @returns {number}
+	 */
+	static getRandomIndexFromArray(arr) {
+		return Math.floor(Math.random() * arr.length);
+	}
 
-    /**
-     * Get a random index from the array given as argument
-     * @param {any[]} arr 
-     * @returns {number}
-     */
-    static getRandomIndexFromArray(arr){
-        return Math.floor(Math.random() * arr.length);
-    }
+	/**
+	 * Given a Date, returns it in string based on format as argument
+	 * @param {Date} theDate
+	 * @param {string} format
+	 * @returns {string}
+	 */
+	static getDateAsString(theDate, format = null) {
+		if (!theDate || !theDate instanceof Date) throw new Error("Date given is not instance of Date");
+		const date = theDate.getDate();
+		const day = Utils.days[theDate.getDay()];
+		const dateDisplay = date < 10 ? `0${date}` : date;
+		const month = theDate.getMonth() + 1;
+		const monthDisplay = month < 10 ? `0${month}` : month;
+		const year = theDate.getFullYear();
+		switch (format) {
+			case Utils.dateFormats.DayMonthYearHyphens:
+				return `${dateDisplay}-${monthDisplay}-${year}`;
+			case Utils.dateFormats.DayMonthYearSlash:
+				return `${dateDisplay}/${monthDisplay}/${year}`;
+			default:
+				return `${day} ${date}, ${Utils.months[month]} ${year}`;
+		}
+	}
 
-    /**
-     * Given a Date, returns it in string based on format as argument
-     * @param {Date} theDate 
-     * @param {string} format 
-     * @returns {string}
-     */
-    static getDateAsString(theDate, format = null){
-        if(!theDate || !theDate instanceof Date) throw new Error("Date given is not instance of Date");
-        const date = theDate.getDate();
-        const day = Utils.days[theDate.getDay()];
-        const dateDisplay = date < 10 ? `0${date}` : date;
-        const month = theDate.getMonth()+1;
-        const monthDisplay = month < 10 ? `0${month}` : month;
-        const year = theDate.getFullYear();
-        switch(format){
-            case Utils.dateFormats.DayMonthYearHyphens:
-                return `${dateDisplay}-${monthDisplay}-${year}`;
-            case Utils.dateFormats.DayMonthYearSlash:
-                return `${dateDisplay}/${monthDisplay}/${year}`;
-            default:
-                return `${day} ${date}, ${Utils.months[month]} ${year}`;
-        }
-    }
+	/**
+	 * Generates a unique id based on random and the date
+	 * @returns {string}
+	 */
+	static genUniqueId() {
+		return Date.now().toString(36) + Math.random().toString(36).substring(2);
+	}
 
-    /**
-     * Generates a unique id based on random and the date
-     * @returns {string}
-     */
-    static genUniqueId(){
-        return Date.now().toString(36) + Math.random().toString(36).substring(2);
-    }
+	/**
+	 * Given a date as string, returns it as Date if the format is correct
+	 * If not, returns itself
+	 * @param {string} dateString
+	 * @param {string} format
+	 * @returns {Date | string}
+	 */
+	static stringAsDate(dateString, format) {
+		if (!format) throw new Error("No format given as arguments");
+		switch (format) {
+			case Utils.dateFormats.DayMonthYearHyphens:
+			case Utils.dateFormats.DayMonthYearSlash:
+				const date = +dateString.substring(0, 2);
+				const month = +dateString.substring(3, 5);
+				const year = +dateString.substring(6);
+				return new Date(year, month - 1, date);
+			default:
+				return dateString;
+		}
+	}
 
-    /**
-     * Given a date as string, returns it as Date if the format is correct
-     * If not, returns itself
-     * @param {string} dateString 
-     * @param {string} format 
-     * @returns {Date | string}
-     */
-    static stringAsDate(dateString, format){
-        if (!format) throw new Error("No format given as arguments");
-        switch (format) {
-            case Utils.dateFormats.DayMonthYearHyphens:
-            case Utils.dateFormats.DayMonthYearSlash:
-                const date = +dateString.substring(0, 2);
-                const month = +dateString.substring(3,5);
-                const year = +dateString.substring(6);
-                return new Date(year, month - 1, date);
-            default:
-                return dateString;
-        }
-    }
+	/**
+	 * Sets the new value of the variable in parameters
+	 * @param {string} nameVariable
+	 * @param {any} newValueVariable
+	 */
+	static setCssVariable(nameVariable, newValueVariable) {
+		const root = document.querySelector(":root");
+		root.style.setProperty(`--${nameVariable}`, newValueVariable);
+	}
 
-    /**
-     * Sets the new value of the variable in parameters
-     * @param {string} nameVariable 
-     * @param {any} newValueVariable 
-     */
-    static setCssVariable(nameVariable, newValueVariable){
-        const root = document.querySelector(":root");
-        root.style.setProperty(`--${nameVariable}`, newValueVariable);
-    }
+	/**
+	 * Gets the CSS variable with its name as argument
+	 * @param {string} nameVariable
+	 * @returns {any} value
+	 */
+	static getCssVariable(nameVariable) {
+		const root = document.querySelector(":root");
+		const rootStyle = getComputedStyle(root);
+		const value = rootStyle.getPropertyValue(`--${nameVariable}`);
+		return value;
+	}
 
-    /**
-     * Gets the CSS variable with its name as argument
-     * @param {string} nameVariable 
-     * @returns {any} value
-     */
-    static getCssVariable(nameVariable){
-        const root = document.querySelector(":root");
-        const rootStyle = getComputedStyle(root);
-        const value = rootStyle.getPropertyValue(`--${nameVariable}`);
-        return value;
-    }
+	/**
+	 * Notify the user with the message in argument
+	 * @param {string} message
+	 * @returns {void}
+	 */
+	static notify(message) {
+		// Notifications not supported
+		if (!("Notification" in window)) return alert("This browser doesn't support Notifications");
+		// User denied the use of Notifications OR no message
+		if (Notification.permission === "denied" || !message) return;
+		const displayNotification = () => new Notification(message);
+		// Not granted for the moment
+		if (Notification.permission !== "granted") {
+			// We request access
+			Notification.requestPermission().then((permission) => {
+				// If user accepts it
+				if (permission === "granted") displayNotification();
+			});
+		}
+		// User granted us access to use Notifications
+		else displayNotification();
+	}
 
-    //#endregion
+	//#endregion
 
-    //#region Private methods
+	//#region Private methods
 
-    //#endregion
-
+	//#endregion
 }
--- projects/countdown-timer/js/app.js
+++ projects/countdown-timer/js/app.js
@@ -1,7 +1,9 @@
-const countdown = document.getElementById('countdown');
-const btnLaunch = document.getElementById('btnLaunch');
-const minutesList = document.getElementById('minutes');
-const secondsList = document.getElementById('seconds');
+import Utils from "../../../js/Utils.js";
+
+const countdown = document.getElementById("countdown");
+const btnLaunch = document.getElementById("btnLaunch");
+const minutesList = document.getElementById("minutes");
+const secondsList = document.getElementById("seconds");
 const secondInMilliseconds = 1000;
 let isLaunched = false;
 let minutes = null;
@@ -13,58 +15,59 @@ const generateSeconds = () => generate60OptionsOnList(secondsList);
 
 /**
  * Display 60 options for the given select element
- * @param {HTMLSelectElement} listHtml 
+ * @param {HTMLSelectElement} listHtml
  */
-const generate60OptionsOnList = (listHtml) => {
-    let res = ``;
-    for (let i = 0; i <= 60; i++) {
-        res += `<option value="${i}">${i}</option>`;
-    }
-    listHtml.innerHTML = res;
-};
+function generate60OptionsOnList(listHtml) {
+	let res = ``;
+	for (let i = 0; i <= 60; i++) {
+		res += `<option value="${i}">${i}</option>`;
+	}
+	listHtml.innerHTML = res;
+}
 
 /* Reset each value necessary to restart */
-const init = () => {
-    isLaunched = false;
-    minutes = null;
-    seconds = null;
-    btnLaunch.style.display = "block";
-    countdown.innerText = "";
-    generateMinutes();
-    generateSeconds();
-};
+function init() {
+	isLaunched = false;
+	minutes = null;
+	seconds = null;
+	btnLaunch.style.display = "block";
+	countdown.innerText = "";
+	generateMinutes();
+	generateSeconds();
+}
 
 /* Hides button, get value of each minutes and seconds */
-const launch = () => {
-    isLaunched = true;
-    countdown.innerText = "";
-    btnLaunch.style.display = "none";
-    minutes = parseInt(minutesList.value);
-    seconds = parseInt(secondsList.value);
-    if(isNaN(minutes) || isNaN(seconds)) return init();
-    launchTimers();
-};
+function launch() {
+	isLaunched = true;
+	countdown.innerText = "";
+	btnLaunch.style.display = "none";
+	minutes = parseInt(minutesList.value);
+	seconds = parseInt(secondsList.value);
+	if (isNaN(minutes) || isNaN(seconds)) return init();
+	launchTimers();
+}
 
 /* Declare the interval, and the end */
-const launchTimers = () => {
-    const inter = setInterval(changeTime, secondInMilliseconds);
-    setTimeout(() => {
-        countdown.innerText = "FINISHED !";
-        btnLaunch.style.display = "block";
-        clearInterval(inter);
-    }, minutes * 60 * secondInMilliseconds + seconds * secondInMilliseconds);
-};
+function launchTimers() {
+	const inter = setInterval(changeTime, secondInMilliseconds);
+	setTimeout(() => {
+		countdown.innerText = "FINISHED !";
+		btnLaunch.style.display = "block";
+		clearInterval(inter);
+		Utils.notify("Countdown finished");
+	}, minutes * 60 * secondInMilliseconds + seconds * secondInMilliseconds);
+}
 
 /* Display remaining time, decrease the minutes and seconds */
-const changeTime = () => {
-    seconds--;
-    if(seconds === 0 && minutes > 0) {
-        seconds = 60;
-        minutes--;
-    }
-    countdown.innerText = `${minutes} : ${seconds}`;
-};
+function changeTime() {
+	seconds--;
+	if (seconds === 0 && minutes > 0) {
+		seconds = 60;
+		minutes--;
+	}
+	countdown.innerText = `${minutes} : ${seconds}`;
+}
 
 /* Attach events */
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 btnLaunch.addEventListener("click", launch);
